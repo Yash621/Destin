3064a5dbf49de51d2337b5b29ac66aba80189759
yash621 <yg17381@gmail.com>
2021-09-04 20:57:20 +0530

    nav bar completed

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
   {
     title: "jdfsakj",
+    image: "https://links.papareact.com/3pn",
+    title: "Get a Ride",
+    screen: "MapScreen",
+  },
+
+  {
+    title: "jdfsakj",
+    image: "https://links.papareact.com/28w",
+    title: "order food",
+    screen: "EatsScreen",
   },
 ];
 
@@ -13,8 +25,15 @@ const NavOptions = () => {
       data={data}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>kdjfska</Text>
+        <TouchableOpacity style={tw`p-2 pl-6  pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon name="arrowright" color="white" type="antdesign" />
+          </View>
         </TouchableOpacity>
       )}
     />
